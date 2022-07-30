7686be2ff02bb11eb7e726973faac6fdbb110d1f
Alex Santos <alexdsantosv@gmail.com>
2022-07-29 22:27:08 -0600

    [ Add ] documentation by country

--- src/pages/documentation/index.jsx
+++ src/pages/documentation/index.jsx
@@ -1,6 +1,8 @@
+// REACT
 import React, { useEffect } from "react";
 
-import { Box, Stack, Text } from "@chakra-ui/react";
+// COMPONENTS
+import CountrySelect from "./components/countrySelect";
 
 const DocumentationPage = () => {
   useEffect(() => {
@@ -8,44 +10,9 @@ const DocumentationPage = () => {
   }, []);
 
   return (
-    <Box padding={{ base: "24px", md: "40px" }} bgColor="blue.500">
-      <Stack
-        gap="40px"
-        alignItems="center"
-        justifyContent="center"
-        direction={{ base: "column", md: "row" }}
-      >
-        <Stack justifyContent="center" alignItems="center" maxWidth="340px">
-          <Text textAlign="center" fontFamily="Montserrat Medium">
-            Decreto Número 44-16 El Congreso de La Republica de Guatemala
-          </Text>
-          <iframe src="/codigo.pdf" height="400px" width="340px"></iframe>
-        </Stack>
-
-        <Stack justifyContent="center" alignItems="center" maxWidth="340px">
-          <Text textAlign="center" fontFamily="Montserrat Medium">
-            Protocolo Nacional de Atención y Protección Integral de la Niñez
-          </Text>
-          <iframe
-            height="400px"
-            width="340px"
-            src="/protocolo_marzo.pdf"
-          ></iframe>
-        </Stack>
-
-        <Stack justifyContent="center" alignItems="center" maxWidth="340px">
-          <Text textAlign="center" fontFamily="Montserrat Medium">
-            Protocolo Nacional para la Recepción y Atención de Niñez y
-            Adolescencia
-          </Text>
-          <iframe
-            height="400px"
-            src="/protocolo_nacional.pdf"
-            width="340px"
-          ></iframe>
-        </Stack>
-      </Stack>
-    </Box>
+    <>
+      <CountrySelect />
+    </>
   );
 };
 
